a768eba1b57d19b56cda38d330d73b77a6a9575f
Tobia Giani <gianitobia@gmail.com>
2014-05-13 17:39:55 +0200

    eliminati i miei commit precedenti

--- Progetti Java/SceneGenerator/src/MenuPanel.java
+++ Progetti Java/SceneGenerator/src/MenuPanel.java
@@ -38,7 +38,6 @@ public class MenuPanel extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
 }
--- Progetti Java/SceneGenerator/src/ScenePanel.java
+++ Progetti Java/SceneGenerator/src/ScenePanel.java
@@ -19,7 +19,7 @@ public class ScenePanel extends javax.swing.JPanel {
      */
     public ScenePanel() {
         initComponents();
-        
+
     }
 
     /**
@@ -45,7 +45,7 @@ public class ScenePanel extends javax.swing.JPanel {
 
     @Override
     public void paintComponent(Graphics g) {
-        System.out.println(this.getWidth() + " "+ this.getHeight());
+        System.out.println(this.getWidth() + " " + this.getHeight());
         super.paintComponent(g); // call superclass's paintComponent  
 
         Graphics2D g2 = (Graphics2D) g; // cast g to Graphics2D  
@@ -58,6 +58,6 @@ public class ScenePanel extends javax.swing.JPanel {
     // End of variables declaration//GEN-END:variables
 
     void init() {
-        s = new Scene(5,5,this.getWidth(), this.getHeight());
+        s = new Scene(5, 5, this.getWidth(), this.getHeight());
     }
 }
