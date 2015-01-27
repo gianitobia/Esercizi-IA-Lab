47364d656f9163f76d248d628c0a6f4989f474fa
Tobia Giani <gianitobia@gmail.com>
2015-01-27 01:03:57 +0100

    convertita la matrice di JLabel in un paintComponent
    Ottimizzate le prestazioni di tutto il programma di Monitor

--- PROGETTO_CLIPS/Progetto Finale/waitor/src/monitor1213/MonitorView.java
+++ PROGETTO_CLIPS/Progetto Finale/waitor/src/monitor1213/MonitorView.java
@@ -1,11 +1,9 @@
 package monitor1213;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.Image;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -15,9 +13,7 @@ import java.util.Observer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import xclipsjni.ClipsView;
@@ -41,9 +37,8 @@ public class MonitorView extends ClipsView implements Observer {
 
     private MonitorModel model;
     private JFrame view;
-    private JPanel mapPanelContainer;
-    private JPanel mapPanel;
-    private JLabel[][] map;
+    private MapPanel mapPanel;
+    //private JLabel[][] map;
     private final int MAP_DIMENSION = 550;
     private final int DEFAULT_IMG_SIZE = 85;
     private JPanel cp_JPanel; // Pannello
@@ -56,7 +51,7 @@ public class MonitorView extends ClipsView implements Observer {
 
     // HashMap che attribuisce ad ogni tipo di cella un codice univoco.
     // L'attribuzione è effettuata nel costruttore.
-    private final Map<String, ImageIcon> map_img;
+    private final Map<String, BufferedImage> map_img;
     private final Map<String, BufferedImage> map_img_robot;
 
     /**
@@ -69,38 +64,41 @@ public class MonitorView extends ClipsView implements Observer {
         //Primo campo: coerente con i file di CLIPS
         //Secondo campo: nome del file (a piacere)
         map_img = new HashMap<>();
-        map_img.put("Wall", new ImageIcon("img" + File.separator + "wall.jpg"));
-        map_img.put("Empty", new ImageIcon("img" + File.separator + "empty.png"));
-        map_img.put("Seat", new ImageIcon("img" + File.separator + "seat.png"));
-        map_img.put("Table_clean", new ImageIcon("img" + File.separator + "table_clean.png"));
-        map_img.put("Table_dirty", new ImageIcon("img" + File.separator + "table_dirty.png"));
-        map_img.put("TB", new ImageIcon("img" + File.separator + "trash_basket.png"));
-        map_img.put("RB", new ImageIcon("img" + File.separator + "recycle_basket.png"));
-        map_img.put("DD", new ImageIcon("img" + File.separator + "drink_dispenser.png"));
-        map_img.put("FD", new ImageIcon("img" + File.separator + "food_dispenser.png"));
-
-        map_img.put("Parking", new ImageIcon("img" + File.separator + "parking.png"));
-
-        //Per l'agente, il primo campo deve essere di tipo agent_<direction>
-        //Dove <direction> è il valore del campo preso da CLIPS.
         map_img_robot = new HashMap<>();
+
         try {
+            map_img.put("Wall", ImageIO.read(new File("img" + File.separator + "wall.jpg")));
+            map_img.put("Empty", ImageIO.read(new File("img" + File.separator + "empty.png")));
+            map_img.put("Seat", ImageIO.read(new File("img" + File.separator + "seat.png")));
+            map_img.put("Table_clean", ImageIO.read(new File("img" + File.separator + "table_clean.png")));
+            map_img.put("Table_dirty", ImageIO.read(new File("img" + File.separator + "table_dirty.png")));
+            map_img.put("TB", ImageIO.read(new File("img" + File.separator + "trash_basket.png")));
+            map_img.put("RB", ImageIO.read(new File("img" + File.separator + "recycle_basket.png")));
+            map_img.put("DD", ImageIO.read(new File("img" + File.separator + "drink_dispenser.png")));
+            map_img.put("FD", ImageIO.read(new File("img" + File.separator + "food_dispenser.png")));
+
+            map_img.put("Parking", ImageIO.read(new File("img" + File.separator + "parking.png")));
+
+            //Per l'agente, il primo campo deve essere di tipo agent_<direction>
+            //Dove <direction> è il valore del campo preso da CLIPS.
             map_img_robot.put("agent_east", ImageIO.read(new File("img" + File.separator + "agent_east.png")));
             map_img_robot.put("agent_west", ImageIO.read(new File("img" + File.separator + "agent_west.png")));
             map_img_robot.put("agent_north", ImageIO.read(new File("img" + File.separator + "agent_north.png")));
             map_img_robot.put("agent_south", ImageIO.read(new File("img" + File.separator + "agent_south.png")));
+
+            //Per gestire le persone. Non vengono sovrapposte, sono vere e proprie celle
+            //Le persone hanno valore correlato nella map: person_<Posizione>_<ident>
+            //Il terzo valore è l'ident della persona (slot del personstatus)
+            //Per poter funzionare, è necessario che vi siano immagini del tipo
+            //<ident>-person_empty.png
+            //Qualora non si trovi tale immagine, ne viene usata una di default
+            //(che ha nome come il secondo campo della mappa)
+            map_img.put("person_Person", ImageIO.read(new File("img" + File.separator + "person_empty.png"))); //Persona in piedi
+            map_img.put("person_Seat", ImageIO.read(new File("img" + File.separator + "person_seat.png"))); //Persona seduta
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //Per gestire le persone. Non vengono sovrapposte, sono vere e proprie celle
-        //Le persone hanno valore correlato nella map: person_<Posizione>_<ident>
-        //Il terzo valore è l'ident della persona (slot del personstatus)
-        //Per poter funzionare, è necessario che vi siano immagini del tipo
-        //<ident>-person_empty.png
-        //Qualora non si trovi tale immagine, ne viene usata una di default
-        //(che ha nome come il secondo campo della mappa)
-        map_img.put("person_Person", new ImageIcon("img" + File.separator + "person_empty.png")); //Persona in piedi
-        map_img.put("person_Seat", new ImageIcon("img" + File.separator + "person_seat.png")); //Persona seduta
 
         model = new MonitorModel();
         model.addObserver((Observer) this);
@@ -187,7 +185,6 @@ public class MonitorView extends ClipsView implements Observer {
 
         int x = mapString.length;
         int y = mapString[0].length;
-        map = new JLabel[x][y];
         int cellDimension = Math.round(MAP_DIMENSION / x);
 
         // bloccata la dimensione massima delle singole immagini
@@ -195,33 +192,10 @@ public class MonitorView extends ClipsView implements Observer {
             cellDimension = DEFAULT_IMG_SIZE;
         }
 
-        mapPanelContainer = new JPanel();
-        mapPanel = new JPanel();
-        mapPanel.setLayout(new GridLayout(x, y));
-        mapPanelContainer.add(mapPanel, BorderLayout.CENTER);
-        mapPanel.setBackground(Color.white);
-
-        //Inserisce in ogni elemento del grid la corretta immagine.
-        for (int i = x - 1; i >= 0; i--) {
-            for (int j = 0; j < y; j++) {
-                ImageIcon icon = map_img.get(mapString[i][j]); //Sfrutta la HashTable per trovare l'immagine correlata al nome
-                if (icon != null) {
-                    Image image = icon.getImage().getScaledInstance(cellDimension, cellDimension, Image.SCALE_SMOOTH);
-                    icon = new ImageIcon(image);
-                    map[i][j] = new JLabel(icon);
-                    map[i][j].setToolTipText("(" + (i + 1) + ", " + (j + 1) + ")");
-                    mapPanel.add(map[i][j]);
-                } else {
-                    map[i][j] = new JLabel(icon);
-                    mapPanel.add(map[i][j]);
-                }
-            }
-            System.out.println();
-        }
-        view.add(mapPanelContainer, BorderLayout.SOUTH);
-
-        view.pack();
-
+        mapPanel = new MapPanel();
+        view.add(mapPanel);
+        view.setSize(689, 800);
+        view.validate();
     }
 
     /**
@@ -236,89 +210,26 @@ public class MonitorView extends ClipsView implements Observer {
         cp_frame.getTimeTextField().setText(time.toString());
         cp_frame.getLeftTimeTextField().setText(leftTime.toString());
         cp_frame.getStepTextField().setText(step.toString());
-
-        String[][] mapString = model.getMap();
-        int cellDimension = Math.round(MAP_DIMENSION / map.length);
-
-        for (int i = map.length - 1; i >= 0; i--) {
-            for (int j = 0; j < map[0].length; j++) {
-                @SuppressWarnings("UnusedAssignment")
-                String direction = "";
-                ImageIcon icon;
-                Image image;
-                BufferedImage background;
-                BufferedImage robot;
-
-                // cerca se, nei primi 6 caratteri (se ce ne sono almeno 6), c'è la stringa "agent_", vedere metodo updateMap in MonitorModel.java
-                // Nel modello si ha una stringa del tipo agent_empty se l'agent si trova su una cella empty.
-                // In modo da inserire l'icona del robot sopra la cella in cui si trova (le due immagini vengono sovrapposte)
-                // ##### SE AGENTE #####
-                if (mapString[i][j].length() >= 6 && mapString[i][j].substring(0, 6).equals("agent_")) {
-                    direction = model.getDirection();
-                    // ...nel, caso prosegue dal 6° carattere in poi.
-
-                    background = imageIcon2Buffered(map_img.get(mapString[i][j].substring(6, mapString[i][j].length())));
-                    robot = map_img_robot.get("agent_" + direction);
-
-                    icon = overlapImages(robot, background);
-
-                    //Imposta il tooltip
-                    map[i][j].setToolTipText("Agent (" + (i + 1) + ", " + (j + 1) + ")");
-
-                    // ##### SE PERSONA #####
-                } else if (mapString[i][j].length() >= 7 && mapString[i][j].substring(0, 7).equals("person_")) {
-                    //Nella forma person_<Background>_<ident>
-                    String map_contains = mapString[i][j];
-                    String[] person_info = map_contains.split("_"); //prendiamo i tre campi
-                    //path dell'immagine apposita (se esiste) per la persona
-
-                    if (map_img.get(person_info[2] + "-" + person_info[0] + "_" + person_info[1]) != null) { //se esiste immagine apposita per quell'id
-                        icon = map_img.get(person_info[2] + "-" + person_info[0] + "_" + person_info[1]);
-                    } else { //Se il file non esiste si usa quello di default (senza ident davanti)
-                        icon = map_img.get(person_info[0] + "_" + person_info[1]);
-                    }
-                    //Imposta il tooltip
-                    map[i][j].setToolTipText("Client " + person_info[2] + " " + "(" + (i + 1) + ", " + (j + 1) + ")");
-
-                    // ##### SE TAVOLO ####
-                } else if (mapString[i][j].length() >= 5 && mapString[i][j].substring(0, 5).equals("Table")) {
-                    //Nella forma Table_<status>_<table-id>
-                    String map_contains = mapString[i][j];
-                    String[] table_info = map_contains.split("_"); //prendiamo i tre campi
-
-                    icon = map_img.get(table_info[0] + "_" + table_info[1]);
-
-                    //Imposta il tooltip
-                    map[i][j].setToolTipText("Table " + table_info[2] + " " + "(" + (i + 1) + ", " + (j + 1) + ")");
-
-                    // ##### ALTRIMENTI ####
-                    // Era una cella che non aveva bisogno di sovrapposizioni e non è una persona
-                } else {
-                    icon = map_img.get(mapString[i][j]);
-                    map[i][j].setToolTipText("(" + (i + 1) + ", " + (j + 1) + ")");
-                }
-                image = icon.getImage().getScaledInstance(cellDimension, cellDimension, Image.SCALE_SMOOTH);
-                icon = new ImageIcon(image);
-                map[i][j].setIcon(icon);
-                map[i][j].repaint();
-            }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
         }
+        mapPanel.repaint();
         DebugFrame.appendText("[SYSTEM] Step attuale: " + model.getStep());
 
     }
 
-    private BufferedImage imageIcon2Buffered(ImageIcon tempicon) {
-        BufferedImage bi = new BufferedImage(
-                tempicon.getIconWidth(),
-                tempicon.getIconHeight(),
-                BufferedImage.TYPE_INT_RGB);
-        Graphics g = bi.createGraphics();
-        // paint the Icon to the BufferedImage.
-        tempicon.paintIcon(null, g, 0, 0);
-        g.dispose();
-        return bi;
-    }
-
+//    private BufferedImage imageIcon2Buffered(ImageIcon tempicon) {
+//        BufferedImage bi = new BufferedImage(
+//                tempicon.getIconWidth(),
+//                tempicon.getIconHeight(),
+//                BufferedImage.TYPE_INT_RGB);
+//        Graphics g = bi.createGraphics();
+//        // paint the Icon to the BufferedImage.
+//        tempicon.paintIcon(null, g, 0, 0);
+//        g.dispose();
+//        return bi;
+//    }
     /**
      * Restituisce l'immagine che è la sovrapposizione fra object e background.
      * La dimensione è quella dell'immagine più piccola
@@ -327,7 +238,7 @@ public class MonitorView extends ClipsView implements Observer {
      * @param background
      * @return
      */
-    private ImageIcon overlapImages(BufferedImage object, BufferedImage background) {
+    private BufferedImage overlapImages(BufferedImage object, BufferedImage background) {
         BufferedImage combined;
         Graphics g;
         // crea una nuova immagine, la dimensione è quella più grande tra le 2 img
@@ -340,7 +251,7 @@ public class MonitorView extends ClipsView implements Observer {
         g.drawImage(background, 0, 0, null);
         g.drawImage(object, 0, 0, null);
 
-        return new ImageIcon(combined);
+        return combined;
     }
 
     @SuppressWarnings("ResultOfObjectAllocationIgnored")
@@ -442,5 +353,81 @@ public class MonitorView extends ClipsView implements Observer {
      */
     public int getVerbosityMode() {
         return verbose_mode;
+    }
+
+    private class MapPanel extends JPanel {
+
+        public MapPanel() {
+            super();
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2 = (Graphics2D) g;
+
+            String[][] mapString = model.getMap();
+            int cellDimension = Math.round(MAP_DIMENSION / mapString.length);
+
+            for (int i = mapString.length - 1; i >= 0; i--) {
+                for (int j = 0; j < mapString[0].length; j++) {
+                    @SuppressWarnings("UnusedAssignment")
+                    String direction = "";
+                    BufferedImage icon;
+                    BufferedImage background;
+                    BufferedImage robot;
+
+                    // cerca se, nei primi 6 caratteri (se ce ne sono almeno 6), c'è la stringa "agent_", vedere metodo updateMap in MonitorModel.java
+                    // Nel modello si ha una stringa del tipo agent_empty se l'agent si trova su una cella empty.
+                    // In modo da inserire l'icona del robot sopra la cella in cui si trova (le due immagini vengono sovrapposte)
+                    // ##### SE AGENTE #####
+                    if (mapString[i][j].length() >= 6 && mapString[i][j].substring(0, 6).equals("agent_")) {
+                        direction = model.getDirection();
+                        // ...nel, caso prosegue dal 6° carattere in poi.
+
+                        background = map_img.get(mapString[i][j].substring(6, mapString[i][j].length()));
+                        robot = map_img_robot.get("agent_" + direction);
+
+                        icon = overlapImages(robot, background);
+
+                        //Imposta il tooltip
+                        //map[i][j].setToolTipText("Agent (" + (i + 1) + ", " + (j + 1) + ")");
+                        // ##### SE PERSONA #####
+                    } else if (mapString[i][j].length() >= 7 && mapString[i][j].substring(0, 7).equals("person_")) {
+                        //Nella forma person_<Background>_<ident>
+                        String map_contains = mapString[i][j];
+                        String[] person_info = map_contains.split("_"); //prendiamo i tre campi
+                        //path dell'immagine apposita (se esiste) per la persona
+
+                        if (map_img.get(person_info[2] + "-" + person_info[0] + "_" + person_info[1]) != null) { //se esiste immagine apposita per quell'id
+                            icon = map_img.get(person_info[2] + "-" + person_info[0] + "_" + person_info[1]);
+                        } else { //Se il file non esiste si usa quello di default (senza ident davanti)
+                            icon = map_img.get(person_info[0] + "_" + person_info[1]);
+                        }
+                        //Imposta il tooltip
+                        //map[i][j].setToolTipText("Client " + person_info[2] + " " + "(" + (i + 1) + ", " + (j + 1) + ")");
+
+                        // ##### SE TAVOLO ####
+                    } else if (mapString[i][j].length() >= 5 && mapString[i][j].substring(0, 5).equals("Table")) {
+                        //Nella forma Table_<status>_<table-id>
+                        String map_contains = mapString[i][j];
+                        String[] table_info = map_contains.split("_"); //prendiamo i tre campi
+
+                        icon = map_img.get(table_info[0] + "_" + table_info[1]);
+
+                        //Imposta il tooltip
+                        //map[i][j].setToolTipText("Table " + table_info[2] + " " + "(" + (i + 1) + ", " + (j + 1) + ")");
+                        // ##### ALTRIMENTI ####
+                        // Era una cella che non aveva bisogno di sovrapposizioni e non è una persona
+                    } else {
+                        icon = map_img.get(mapString[i][j]);
+                        //map[i][j].setToolTipText("(" + (i + 1) + ", " + (j + 1) + ")");
+                    }
+
+                    g2.drawImage(icon, cellDimension * j, cellDimension * (mapString.length - i), cellDimension, cellDimension, this);
+
+                }
+            }
+
+        }
     }
 }
