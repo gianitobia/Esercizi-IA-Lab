9c2617b34c3565484d1f6177252b255e02bb7190
Tobia Giani <gianitobia@gmail.com>
2015-05-01 19:26:47 +0200

    aumentato lo spazio sotto

--- PROGETTO_CLIPS/Progetto Finale/waitor/src/monitor1213/MonitorView.java
+++ PROGETTO_CLIPS/Progetto Finale/waitor/src/monitor1213/MonitorView.java
@@ -382,7 +382,7 @@ public class MonitorView extends ClipsView implements Observer {
             }
 
             int x0 = (this.getWidth() - cellWidth * mapString.length) / 2;
-            int y0 = (this.getHeight() - 20 - cellHeight * mapString[0].length) / 2;
+            int y0 = (this.getHeight() - 30 - cellHeight * mapString[0].length) / 2;
 
             for (int i = mapString.length - 1; i >= 0; i--) {
                 g2.drawString((i + 1) + "", x0 - cellWidth, y0 + cellHeight / 2 + cellHeight * (mapString.length - i));
